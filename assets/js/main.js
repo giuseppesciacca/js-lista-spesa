ca7f91f3e6d1a543a5c83772f258d27a1ba288c4
giuseppesciacca <sciaccagi@gmail.com>
2023-03-15 15:12:54 +0100

    [add] input and btn in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,29 @@ const shoppingList = [];
 
 const ulEl = document.createElement('ul');
 const listSectEl = document.getElementById('list_sec');
+const buyEl = document.getElementById('acquisto');
+console.log(buyEl.value);
+const btnEl = document.getElementById('submit');
 
 listSectEl.append(ulEl);        //metto l'<ul> nella section
 
-while (true) {
-    const liEl = document.createElement('li');
-    const element = prompt('Cosa vuoi comprare?');
+btnEl.addEventListener('click', function (e) {
+    e.preventDefault;
 
-    if (element == "stop") {    //se scrivo stop si ferma
-        break
+    shoppingList.push(buyEl.value); //metto ciò che scrivo nella lista
+    buyEl.value = '';
+    console.log(shoppingList);
+
+    const liEl = document.createElement('li'); //ad ogni click <li>
+
+    let i = 0;
+    while (i < shoppingList.length) {
+        const thisEl = shoppingList[i];
+
+        liEl.innerHTML = thisEl;  //metto ciò che scrivo nell'input
+        ulEl.append(liEl);          //metto gli <li> nell'<ul>
+
+        i++;
     };
-
-    shoppingList.push(element); //metto ciò che scrivo nella lista
-    liEl.innerHTML += element;  //metto ciò che scrivo negli <li>
-    ulEl.append(liEl);          //metto gli <li> nell'<ul>
-};
+});
 console.log(shoppingList);
