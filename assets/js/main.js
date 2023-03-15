f4b48b5e6c9797f0f69ef3b2b0394b0e14c2dbfd
giuseppesciacca <sciaccagi@gmail.com>
2023-03-15 14:32:28 +0100

    [add] list in <li> with while loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 /* Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while. */
 const shoppingList = [];
 
+const ulEl = document.createElement('ul');
+const listSectEl = document.getElementById('list_sec');
+
+listSectEl.append(ulEl);        //metto l'<ul> nella section
+
 while (true) {
-    const element = prompt('Cosa vuoi comprare? Quando hai finito digita "stop"');
-    if (element == "stop") {
+    const liEl = document.createElement('li');
+    const element = prompt('Cosa vuoi comprare?');
+
+    if (element == "stop") {    //se scrivo stop si ferma
         break
     };
-    shoppingList.push(element);
+
+    shoppingList.push(element); //metto ciò che scrivo nella lista
+    liEl.innerHTML += element;  //metto ciò che scrivo negli <li>
+    ulEl.append(liEl);          //metto gli <li> nell'<ul>
 };
 console.log(shoppingList);
